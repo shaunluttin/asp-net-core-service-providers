73919dbc3a8409652a126c290db1822cd7cfcc8c
Shaun Luttin <shaunluttin@gmail.com>
2018-08-28 18:49:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Call `dotnet run` and then look at the console output.
---
+++ Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class SomeSingltonService { }
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+        services.AddSingleton<SomeSingltonService>();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        var appServices = app.ApplicationServices;
+        var appService = appServices.GetRequiredService<SomeSingltonService>();
+
+        Console.WriteLine("=======================");
+        Console.WriteLine("Configure");
+
+        app.UseMvc(configureRoutes =>
+        {
+            var routeServices = configureRoutes.ServiceProvider;
+            var routeService = routeServices.GetRequiredService<SomeSingltonService>();
+
+            Console.WriteLine("UseMvc");
+
+            if (appServices == routeServices && appService == routeService)
+            {
+                Console.WriteLine("They are the same instances.");
+            }
+        });
+
+        Console.WriteLine("=======================");
+    }
+}
